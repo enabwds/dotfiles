658e45df5df88c02fc44427ad50742884d443b4c
Yohann Nihalani <nihalaniyohann15@gmail.com>
2024-10-18 23:22:02 +0530

    Added a deploy script

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+rm -rf ~/.emacs*
+rm ~/.vimrc
+rm ~/.zshrc
+rm ~/.bashrc
+rm ~/.ghci
+rm ~/.tmux.conf
+rm -rf ~/.config/ags
+rm -rf ~/.config/cava
+rm -rf ~/.config/fastfetch
+rm -rf ~/.config/fish
+rm -rf ~/.config/htop
+rm -rf ~/.config/hypr
+rm -rf ~/.config/i3
+rm -rf ~/.config/kitty
+rm -rf ~/.config/nvim
+rm -rf ~/.config/picom
+rm -rf ~/.config/polybar
+rm -rf ~/.config/waybar
+
+sudo pacman -Syu stow
+
+stow .
