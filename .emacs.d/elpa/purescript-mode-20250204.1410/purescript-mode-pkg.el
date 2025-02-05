19531de5d318cfd353f9e7d680b74f63becc4747
Yohann Nihalani <nihalaniyohann15@gmail.com>
2025-02-05 19:59:52 +0530

    updated emacs packages :3c

---
+++ .emacs.d/elpa/purescript-mode-20250204.1410/purescript-mode-pkg.el
@@ -0,0 +1,16 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "purescript-mode" "20250204.1410"
+  "A PureScript editing mode."
+  '((emacs "25.1"))
+  :url "https://github.com/purescript-emacs/purescript-mode"
+  :commit "e0de552627487e4c6613318e42d4402563362d28"
+  :revdesc "e0de55262748"
+  :keywords '("faces" "files" "purescript")
+  :authors '(("1997-1998 Graeme E Moss and" . "gem@cs.york.ac.uk")
+             ("Tommy Thorn" . "thorn@irisa.fr")
+             ("2003 Dave Love" . "fx@gnu.org")
+             ("2014 Tim Dysinger" . "tim@dysinger.net"))
+  :maintainers '(("1997-1998 Graeme E Moss and" . "gem@cs.york.ac.uk")
+                 ("Tommy Thorn" . "thorn@irisa.fr")
+                 ("2003 Dave Love" . "fx@gnu.org")
+                 ("2014 Tim Dysinger" . "tim@dysinger.net")))
---
+++ .emacs.d/elpa/racket-mode-20250201.1412/racket-mode-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "racket-mode" "20250201.1412"
+  "Racket editing, REPL, and more."
+  '((emacs "25.1"))
+  :url "https://www.racket-mode.com/"
+  :commit "937ae8e207684d762619bcb6a30ee14a052636da"
+  :revdesc "937ae8e20768"
+  :authors '(("Greg Hendershott" . "racket-mode-author@greghendershott.com")))
